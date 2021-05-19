a1719585b777c3abda594e5aaf7f80d09a15286e
samuFerragut <sferragut@cifpfbmoll.eu>
2021-05-19 11:58:02 +0200

    feat: Create header

--- src/components/header.js
+++ src/components/header.js
@@ -26,7 +26,7 @@ export default function Header() {
         <navbar>
             <img src={logo} className="logo"></img>
             <div className="hamburger">
-                <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleclick}>
+                <Button className="button" aria-controls="simple-menu" aria-haspopup="true" onClick={handleclick}>
                     Menu
                 </Button>
                 <Menu 
@@ -35,11 +35,22 @@ export default function Header() {
                     keepMounted
                     open={Boolean(anchorE1)}
                     onClose={handleClose}
+                    className="menu"
                 >
                     <MenuItem onClick={handleClose}>Inicia Sesión</MenuItem>
                     <MenuItem onClick={handleClose}>Ayuda</MenuItem>
                     <MenuItem onClick={handleClose}>Selecciona tu país</MenuItem>
                 </Menu>
+            </div>
+            <div className="search">
+                <br></br>
+                <h1>Pide lo que te pida el cuerpo</h1>
+                <p>Comida a domicilio online cerca de ti</p>
+                <form>
+                    <input type="text"></input>
+                    <button type="submit">Buscar restaurantes</button>
+                </form>
+                <br></br>
             </div>
         </navbar>
     );
