c60c47bfd551498ef451d051bc77a4749d30c0fe
samuFerragut <sferragut@cifpfbmoll.eu>
2021-05-21 18:09:27 +0200

    fix: Fix header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import Button from "@material-ui/core/Button"
-import Menu from "@material-ui/core/Menu"
-import MenuItem from "@material-ui/core/MenuItem"
+import React, { Component } from 'react';
 
 /** IMAGES */
 import bg from '../assets/images/bg.jpg';
@@ -10,49 +7,36 @@ import logo from '../assets/images/Logos_Just-eat.png';
 /** CSS */
 import '../assets/css/header.css';
 
-export default function Header() {
+class Header extends Component {
 
-    const [anchorE1, setAnchorE1] = React.useState(null);
-
-    const handleclick = (event) => {
-        setAnchorE1(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorE1(null);
-    };
-
-    return (
-        <navbar>
-            <img src={logo} className="logo"></img>
-            <div className="hamburger">
-                <Button className="button" aria-controls="simple-menu" aria-haspopup="true" onClick={handleclick}>
-                    Menu
-                </Button>
-                <Menu 
-                    id="simple-menu"
-                    anchorE1={anchorE1}
-                    keepMounted
-                    open={Boolean(anchorE1)}
-                    onClose={handleClose}
-                    className="menu"
-                >
-                    <MenuItem onClick={handleClose}>Inicia Sesión</MenuItem>
-                    <MenuItem onClick={handleClose}>Ayuda</MenuItem>
-                    <MenuItem onClick={handleClose}>Selecciona tu país</MenuItem>
-                </Menu>
-            </div>
-            <div className="search">
-                <br></br>
-                <h1>Pide lo que te pida el cuerpo</h1>
-                <p>Comida a domicilio online cerca de ti</p>
-                <form>
-                    <input type="text"></input>
-                    <button type="submit">Buscar restaurantes</button>
-                </form>
-                <br></br>
-            </div>
-        </navbar>
-    );
+    render() {
+        return (
+            <navbar>
+                <a href="/"><img src={logo} className="logo"></img></a>
+                <nav className="menu">
+                    <ul>
+                        <li>
+                            <a href="/login">Inicia sesión</a>
+                        </li>
+                        <li>
+                            <a href="#">Ayuda</a>
+                        </li>
+                    </ul>
+                </nav>
+                {/** CLEAR FLOATS */}
+                <div className="clearfix"></div>
+                <div className="search">
+                    <br></br>
+                    <h1>Pide lo que te pida el cuerpo</h1>
+                    <p>Comida a domicilio online cerca de ti</p>
+                    <form>
+                        <input type="text"></input>
+                        <button type="submit">Buscar restaurantes</button>
+                    </form>
+                    <br></br>
+                </div>
+            </navbar>
+        );
+    }
 }
-
+export default Header;
